1306420a1e957c5e10efc49a4b81e96b3e55bbcb
jhon11390 <meljhon2@gmail.com>
2020-01-03 22:18:03 -0500

    Se inicia proyecto de suma

---
+++ solution.rb
@@ -0,0 +1,21 @@
+require "sinatra"
+
+datos = [1]
+
+def aumentar(a)
+    valor = a[0]+1
+    a.delete_at(0)
+    a.push(valor)
+    puts valor
+end
+
+
+get "/" do
+    @numero = datos[0]
+    erb :index
+end
+
+post "/hola" do
+    aumentar(datos)
+    redirect "/"
+end
